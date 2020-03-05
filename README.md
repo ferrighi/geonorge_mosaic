bd928ce33f0fb5450b5d7c21af249483509d498c
Lara Ferrighi <laraf@met.no>
2020-03-05 12:11:40 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+## Geonorge Mosaic
+
+Author: Lara Ferrighi, laraf@met.no
+
+This is a drupal module for visualization of Sentinel 2 mosaic as provided by Geonorge. 
+
+## How to install the module
+* Download the module and place it within the module folder. 
+* Create a basic page with URL alias (for example "geonorge-mosaic")
+* Activate the geonorge mosaic block to be visible in the content area of the page you have created 
---
+++ js/geonorge_mosaic.js
@@ -0,0 +1,78 @@
+proj4.defs('EPSG:32661', '+proj=stere +lat_0=90 +lat_ts=90 +lon_0=0 +k=0.994 +x_0=2000000 +y_0=2000000 +datum=WGS84 +units=m +no_defs');
+ol.proj.proj4.register(proj4);
+
+var proj32661 = new ol.proj.Projection({
+  code: 'EPSG:32661',
+  extent: [-4e+06,-6e+06,8e+06,8e+06]
+});
+
+var prj = proj32661;
+
+//import settings from configuration
+var zoom = Drupal.settings.zoom;
+var center_lat = Drupal.settings.center_lat;
+var center_lon = Drupal.settings.center_lon;
+
+var layer = {};
+
+// Base layer WMS
+layer['base']  = new ol.layer.Tile({
+   type: 'base',
+   source: new ol.source.TileWMS({ 
+       url: 'http://public-wms.met.no/backgroundmaps/northpole.map',
+       params: {'LAYERS': 'world', 'TRANSPARENT':'true', 'VERSION':'1.1.1','FORMAT':'image/png', 'SRS':prj}
+   })
+});
+
+// Border layer WMS
+layer['border']  = new ol.layer.Tile({
+   source: new ol.source.TileWMS({
+       url: 'http://public-wms.met.no/backgroundmaps/northpole.map',
+       params: {'LAYERS': 'borders', 'TRANSPARENT':'true', 'VERSION':'1.1.1','FORMAT':'image/png', 'SRS':prj}
+   })
+});
+
+layer['gn_mosaic']  = new ol.layer.Tile({
+       source: new ol.source.TileWMS({
+       url: "http://wms.geonorge.no/skwms1/wms.sentinel2mosaikk",
+       params: {'LAYERS': 'mosaikk,rutenett', 
+                'TRANSPARENT':'true', 
+                'FORMAT':'image/png', 
+                'CRS':'EPSG:25833', 
+                'VERSION':'1.3.0', 
+                'SERVICE':'WMS',
+                'REQUEST':'GetMap',
+                'TILE':'true','WIDTH':'256','HEIGHT':'256'}
+   })
+});
+
+// build up the map 
+var centerLonLat1 = [center_lon, center_lat];
+var centerTrans1 = ol.proj.transform(centerLonLat1, "EPSG:4326",  prj);
+
+var map = new ol.Map({
+   controls: ol.control.defaults().extend([
+      new ol.control.FullScreen()
+   ]),
+   target: 'map',
+   layers: [ layer['base'],
+             layer['gn_mosaic']
+           ],
+   view: new ol.View({
+                 zoom: zoom, 
+                 minZoom: 1,
+                 maxZoom: 10,
+                 center: centerTrans1,
+                 projection: prj
+   })
+});
+
+//Mouseposition
+var mousePositionControl = new ol.control.MousePosition({
+   coordinateFormat : function(co) {
+      return ol.coordinate.format(co, template = 'lon: {x}, lat: {y}', 2);
+   },
+   projection : 'EPSG:4326',
+});
+map.addControl(mousePositionControl);
+
